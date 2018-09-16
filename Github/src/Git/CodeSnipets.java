095d5fa7ff3f3cef76471e78ff62f80a3c1b1b94
Vishal <Vishalauti143@gmail.com>
2018-09-16 14:34:50 +0530

    message updated

--- Github/src/Git/CodeSnipets.java
+++ Github/src/Git/CodeSnipets.java
@@ -1,11 +1,17 @@
 package Git;
 
+
+
+
+
+
+
 public class CodeSnipets {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Will make strong come back");
-				
+		System.out.println("test");
+			
 			
 	}
 }
